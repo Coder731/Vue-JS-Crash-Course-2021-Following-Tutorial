570d75e4d4e0cc3ac02fda5325fe406e27ddcd61
Coder731 <stephenobrien1@gmail.com>
2021-08-06 09:51:06 +0000

    Comment on declarative rendering

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  const app = Vue.createApp({
-     template: '<h1>Hello {{firstName}}</h1>', // double curly braces, similar to interpolation in React
+     template: '<h1>Hello {{firstName}}</h1>', // declarative rendering double curly braces, similar to interpolation in React
      data() { // a function that returns an object
          return {
              firstName: 'John',
